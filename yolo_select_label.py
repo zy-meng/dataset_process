02366ec244fcad9ecc23ab4e3863efd01cb35487
mziyu <mziyuu@foxmail.com>
2022-06-29 18:37:01 +0800

    transport images at same time

--- yolo_select_label.py
+++ yolo_select_label.py
@@ -1,4 +1,6 @@
 import os
+from tqdm import tqdm
+import shutil
 """
 --Annotations
  |-yolo_select_label.py
@@ -9,19 +11,25 @@ import os
  |-train
  |-val
 """
-old_label_dir = "../labels/train"
-new_label_dir = "../labels/new_train"
+root_dir = "/home/mzy/datasets/COCO"
+old_label_dir = os.path.join(root_dir, "labels/train")
+new_label_dir = os.path.join(root_dir, "labels/new_train")
+old_image_dir = os.path.join(root_dir, "images/train")
+new_image_dir = os.path.join(root_dir, "images/new_train")
+
 # old_label_dir = "../labels/val"
 # new_label_dir = "../labels/new_val"
 
 TOTAL_NUM = 0
 DEPRECATE_NUM = 0
 
-if not os.path.exists(new_label_dir):
-    os.mkdir(new_label_dir)
-
 
 def drop_label(file_name):
+    """ Select label for specific class 
+    """
+    global TOTAL_NUM
+    global DEPRECATE_NUM
+
     old_file_path = os.path.join(old_label_dir,file_name)
     with open (old_file_path, "r") as f:
         labels = f.readlines()
@@ -43,12 +51,45 @@ def drop_label(file_name):
     TOTAL_NUM += 1
 
 
+def transport_image(image_name):
+    """ Transport image to new directory
+    """
+    old_image_path = os.path.join(old_image_dir, image_name)
+    new_image_path = os.path.join(new_image_dir, image_name)
+    shutil.copyfile(old_image_path, new_image_path)
+
+
+def mkdir_if_not_exist(dirs):
+    """make directories if not exist, support dir list 
+    """
+    if not isinstance(dirs, list):
+        dirs = [dirs]
+    for dir in dirs:
+        if not os.path.exists(dir):
+            os.mkdir(dir)
+            print("create directory:{}".format(dir))
+
 def main():
+
+    mkdir_if_not_exist([new_label_dir, new_image_dir])
+
     label_name_list = os.listdir(old_label_dir)
-    for label_name in label_name_list:
+    for label_name in tqdm(label_name_list):
         drop_label(label_name)
-    print(TOTAL_NUM)
     
+    image_name_list = os.listdir(new_label_dir)
+    for i in range(len(image_name_list)):
+        image_name_list[i] = image_name_list[i][:-3] + "jpg"
+
+    for image_name in tqdm(image_name_list):
+        transport_image(image_name)
+    
+    print("TOTAL_NUM:{}".format(TOTAL_NUM))
+    print("DEPRECATE_NUM:{}".format(DEPRECATE_NUM))
+
+
+
+
 if __name__ == "__main__":
     main()
 
